2f1029a14f0e4b7979d532bdbb80a017f0fc722a
Jigar <ubuntu@ip-172-31-11-251.ec2.internal>
2017-05-12 22:45:41 +0000

    no the code runs, not functionally correct

--- exampleInference.py
+++ exampleInference.py
@@ -6,7 +6,6 @@ import sys
 import numpy as np
 import pydensecrf.densecrf as dcrf
 import ipdb
-
 # Get im{read,write} from somewhere.
 try:
     from cv2 import imread, imwrite
@@ -36,18 +35,14 @@ fn_output = sys.argv[3]
 img = imread(fn_im)
 
 # Convert the annotation's RGB color to a single 32-bit integer color 0xBBGGRR
-anno_rgb = imread(fn_anno,0).astype(np.int32)
+anno_rgb = imread(fn_anno).astype(np.uint32)
 ipdb.set_trace()
-anno_rgb[anno_rgb>=5] = 1
-anno_rgb += 1
-anno_rgb[0,0] = 0
-#anno_lbl = anno_rgb[:,:,0] + (anno_rgb[:,:,1] << 8) + (anno_rgb[:,:,2] << 16)
-anno_lbl = anno_rgb
+anno_lbl = anno_rgb[:,:,0] + (anno_rgb[:,:,1] << 8) + (anno_rgb[:,:,2] << 16)
+
 # Convert the 32bit integer color to 1, 2, ... labels.
 # Note that all-black, i.e. the value 0 for background will stay 0.
-#colors, labels = np.unique(anno_lbl, return_inverse=True)
-colors = np.unique(anno_lbl)
-labels = anno_lbl
+colors, labels = np.unique(anno_lbl, return_inverse=True)
+
 # But remove the all-0 black, that won't exist in the MAP!
 HAS_UNK = 0 in colors
 if HAS_UNK:
@@ -56,6 +51,12 @@ if HAS_UNK:
     colors = colors[1:]
 #else:
 #    print("No single full-black pixel found in annotation image. Assuming there's no 'unknown' label!")
+
+# And create a mapping back from the labels to 32bit integer colors.
+colorize = np.empty((len(colors), 3), np.uint8)
+colorize[:,0] = (colors & 0x0000FF)
+colorize[:,1] = (colors & 0x00FF00) >> 8
+colorize[:,2] = (colors & 0xFF0000) >> 16
 
 # Compute the number of classes in the label image.
 # We subtract one because the number shouldn't include the value 0 which stands
@@ -67,7 +68,7 @@ print(n_labels, " labels", (" plus \"unknown\" 0: " if HAS_UNK else ""), set(lab
 ### Setup the CRF model ###
 ###########################
 use_2d = False
-#use_2d = True
+# use_2d = True
 if use_2d:
     print("Using 2D specialized functions")
 
@@ -78,22 +79,12 @@ if use_2d:
     U = unary_from_labels(labels, n_labels, gt_prob=0.7, zero_unsure=HAS_UNK)
     d.setUnaryEnergy(U)
 
-    ## This adds the color-independent term, features are the locations only.
-    #d.addPairwiseGaussian(sxy=(3, 3), compat=3, kernel=dcrf.DIAG_KERNEL,
-                          #normalization=dcrf.NORMALIZE_SYMMETRIC)
-
-    ## This adds the color-dependent term, i.e. features are (x,y,r,g,b).
-    #d.addPairwiseBilateral(sxy=(80, 80), srgb=(13, 13, 13), rgbim=img,
-                           #compat=10,
-                           #kernel=dcrf.DIAG_KERNEL,
-                           #normalization=dcrf.NORMALIZE_SYMMETRIC)
-
     # This adds the color-independent term, features are the locations only.
-    d.addPairwiseGaussian(sxy=(10, 10), compat=3, kernel=dcrf.DIAG_KERNEL,
+    d.addPairwiseGaussian(sxy=(3, 3), compat=3, kernel=dcrf.DIAG_KERNEL,
                           normalization=dcrf.NORMALIZE_SYMMETRIC)
 
     # This adds the color-dependent term, i.e. features are (x,y,r,g,b).
-    d.addPairwiseBilateral(sxy=(50, 50), srgb=(20, 20, 20), rgbim=img,
+    d.addPairwiseBilateral(sxy=(80, 80), srgb=(13, 13, 13), rgbim=img,
                            compat=10,
                            kernel=dcrf.DIAG_KERNEL,
                            normalization=dcrf.NORMALIZE_SYMMETRIC)
@@ -133,8 +124,8 @@ MAP = np.argmax(Q, axis=0)
 
 # Convert the MAP (labels) back to the corresponding colors and save the image.
 # Note that there is no "unknown" here anymore, no matter what we had at first.
-ipdb.set_trace()
-imwrite(fn_output, MAP.reshape(img.shape[0], img.shape[1]))
+MAP = colorize[MAP,:]
+imwrite(fn_output, MAP.reshape(img.shape))
 
 # Just randomly manually run inference iterations
 Q, tmp1, tmp2 = d.startInference()
